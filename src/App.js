aedf5a9c58fa15324381eb5d572c88bf4f71fe11
Alok Kumar <aloknh31@gmail.com>
2024-04-27 11:44:29 +0530

    doctype romove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
 
+import "./App.css";
+
+import { filterData, apiUrl } from "./store/Data";
+import { toast } from "react-toastify";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setCourses(data.data);
+    } catch (error) {
+      toast.error("Error in fetching data");
+    }
+
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen bg-bgWhite ">
+    <div>
+      <NavBar />
+    </div>
+
+    <div>
+      <div>
+        <Filter
+          filterData={filterData}
+          category={category}
+          setCategory={setCategory}
+        />
+      </div>
+
+      <div
+        className="w-11/12 max-w-[1200px] 
+      mx-auto flex flex-wrap mt-4 justify-center items-center min-h-[50vh]"
+      >
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <Cards courses={courses} category={category} />
+        )}
+      </div>
+    </div>
+  </div>
+);
+};
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <nav className='bg-gray-800 py-4' >
+        <h1 className='text-white text-3xl font-bold text-center '>
+            Top Courses
+        </h1>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
